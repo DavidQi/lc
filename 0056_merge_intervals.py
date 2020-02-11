a74b1b9a69eea4bc8137fdc6f94fbdb82a3dc844
David Qi <david@job-search-express.com>
2020-02-11 11:31:15 -0500

    Commit

--- 0056_merge_intervals.py
+++ 0056_merge_intervals.py
@@ -2,13 +2,21 @@ class Solution(object):
     def merge(self, intervals):
         if not intervals:
             return []
-        intervals = sorted(intervals, key=lambda x: x.start)
+        intervals = sorted(intervals, key=lambda x: x[0])
 
         result = [intervals[0]]
         for interval in intervals:
-            if interval.start > result[-1].end:
+            if interval[0] > result[-1][-1]:
                 result.append(interval)
-            elif result[-1].end < interval.end:
-                result[-1].end = interval.end
+            elif result[-1][-1] < interval[-1]:
+                result[-1][-1] = interval[-1]
         return result
-        
+
+
+if __name__ == '__main__':
+    l1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
+    l2 = [[-25, -14], [-21, -16], [-20, -15], [-10, -7], [-8, -5], [-6, -3], [2, 4], [2, 3], [3, 6], [12, 15], [13, 18],
+          [14, 17], [22, 27], [25, 30], [26, 29]]
+    o = Solution()
+    print(o.merge(l1))
+    print(o.merge(l2))
